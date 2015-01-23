9832a63b0da59cdae35ea5d79295c1f5dd11daf3
dandersonaspect <david.anderson@aspectsecurity.com>
2015-01-23 10:48:58 -0500

    Added a placeholder unit test for AppScanSourcePlugin.

---
+++ sif-appscansource-plugin/src/test/java/org/sifappscanplugin/sensor/AppScanSourcePluginTest.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package org.sifappscanplugin.sensor;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for the AppScanSourcePlugin class.
+ * 
+ */
+public class AppScanSourcePluginTest
+{
+
+	/**
+	 * Test method for
+	 * {@link org.sifappscanplugin.sensor.AppScanSourcePlugin#sense(org.sif.Job, org.sif.Notifier, java.util.Map)}
+	 * .
+	 */
+	@Test
+	public void testSense()
+	{
+		fail( "Not yet implemented" );
+	}
+
+
+	/**
+	 * Test method for
+	 * {@link org.sifappscanplugin.sensor.AppScanSourcePlugin#report(org.sif.Job, org.sif.Notifier, java.util.Map)}
+	 * .
+	 */
+	@Test
+	public void testReport()
+	{
+		fail( "Not yet implemented" );
+	}
+
+
+	/**
+	 * Test method for
+	 * {@link org.sifappscanplugin.sensor.AppScanSourcePlugin#publish(org.sif.Job, org.sif.Notifier, java.util.Map)}
+	 * .
+	 */
+	@Test
+	public void testPublishJobNotifierMapOfStringString()
+	{
+		fail( "Not yet implemented" );
+	}
+
+
+	/**
+	 * Test method for
+	 * {@link org.sifappscanplugin.sensor.AppScanSourcePlugin#doGetFolderId(java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, boolean)}
+	 * .
+	 */
+	@Test
+	public void testDoGetFolderId()
+	{
+		fail( "Not yet implemented" );
+	}
+
+}
